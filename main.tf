1565050ca09528b111a3df93b26ece3bea4c256c
Aswin Sagar <87416215+aswinsagar12@users.noreply.github.com>
2025-01-06 13:42:01 +0530

    Add Security Group

--- main.tf
+++ main.tf
@@ -2,6 +2,25 @@ provider "aws" {
   region = "us-west-1"
 }
 
+resource "aws_security_group" "instance_sg" {
+  name_prefix = "allow-ssh-"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Launch an EC2 instance
 resource "aws_instance" "web_server" {
   ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 AMI
   instance_type = "t2.micro"
@@ -11,6 +30,6 @@ resource "aws_instance" "web_server" {
 
   # Add a tag
   tags = {
-    Name = "WebServer"
+    Name = "MyWebServer"
   }
 }
